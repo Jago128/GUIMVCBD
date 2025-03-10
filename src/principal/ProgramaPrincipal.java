61362549bac1542454f83f303c117130f86845b6
Jago128 <jagobaetxegarai@gmail.com>
2025-03-10 13:04:48 +0100

    e

--- src/principal/ProgramaPrincipal.java
+++ src/principal/ProgramaPrincipal.java
@@ -1,13 +1,11 @@
 package principal;
-import vista.*;
+
 import controlador.LoginControlador;
-import modelo.*;
 
 public class ProgramaPrincipal {
 	
 	public static void main(String[] args) {
 		LoginControlador cont = new LoginControlador();
 		cont.visualizarPantalla();
-	
 	}
 }
