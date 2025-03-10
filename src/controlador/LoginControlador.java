090dd1689ce5688276287abfc12d01c2e513b728
Jago128 <jagobaetxegarai@gmail.com>
2025-03-10 13:27:00 +0100

    e

--- src/controlador/LoginControlador.java
+++ src/controlador/LoginControlador.java
@@ -5,24 +5,19 @@ import modelo.Usuario;
 import modelo.UsuarioDAO;
 import vista.VentanaLogin;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Map;
-
 public class LoginControlador {
-	UsuarioDAO dao = new ImplementacionBD();
-	
+	UsuarioDAO dao=new ImplementacionBD();
+
 	public void visualizarPantalla() {
-		VentanaLogin ven = new VentanaLogin(this);
+		VentanaLogin ven=new VentanaLogin(this);
 		ven.setVisible(true);	
 	}
+
 	public boolean comprobarUsuario(Usuario usuario){
 		return dao.comprobarUsuario(usuario);	
 	}
+
 	public boolean insertarUsuario(Usuario usuario) {
 		return dao.insertarUsuario(usuario);
 	}
-	
-
-
 }
